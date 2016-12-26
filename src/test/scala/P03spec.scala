2c41b45fa16c6cdd75c731da09f4ea8afc986129
Marek Jeszka <marek.jeszka@gmail.com>
2016-12-26 10:43:51 +0100

    Spec description fixed

--- src/test/scala/P03spec.scala
+++ src/test/scala/P03spec.scala
@@ -3,7 +3,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class P03spec extends FlatSpec with Matchers {
   val p03 = new P03[Int]()
 
-  "P02" should "find the last but one element of a list" in {
+  "P03" should "find the n-th element of a list" in {
     p03.nth(2, List(1, 1, 2, 3, 5, 8)) should be (Some(2))
     p03.nth(0, List(1, 1, 2, 3, 5, 8)) should be (Some(1))
     p03.nth(5, List(1, 1, 2, 3, 5, 8)) should be (Some(8))
